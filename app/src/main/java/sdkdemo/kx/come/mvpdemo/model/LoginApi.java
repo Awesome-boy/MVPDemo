9494709920e714efb7adbf429e22c09ec477c650
zhoutao <zhoutao@coshine.com>
2019-11-19 10:31:49 +0800

    引入retrofit

--- app/src/main/java/sdkdemo/kx/come/mvpdemo/model/LoginApi.java
+++ app/src/main/java/sdkdemo/kx/come/mvpdemo/model/LoginApi.java
@@ -1,11 +1,16 @@
 package sdkdemo.kx.come.mvpdemo.model;
 
 
+import retrofit2.http.POST;
+import rx.Observable;
+import sdkdemo.kx.come.mvpdemo.constant.Urls;
 
 /**
  * Created by issuser on 2018/4/16.
  */
 
 public interface LoginApi {
+
+
 
 }
